4dfd61a578cbde95d726d7d96b4662e05ec4563c
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-19 13:26:29 +0100

    Create little nametag component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { IdComponent } from './components/static/id/id.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    IdComponent
   ],
   imports: [
     BrowserModule,
