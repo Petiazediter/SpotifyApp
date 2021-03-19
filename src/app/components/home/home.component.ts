23f5f905c97fc5bfa8f1392c86d197f71a44f26e
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-19 16:28:51 +0100

    Create green wave to the bottom of the home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service : SpotifyService) { }
 
   ngOnInit(): void {
   }
 
   onConnect(): void{
     console.log("Clicked")
+    //this.service.generateLink("asd","/kaka");
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -4,8 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SpotifyService {
+
+  private clientId : string = 'aaeea3eaae8940c1a35e4645a2028096'
+  private secretKey : string = '554b9e9fca874f1fa0a3ee5ff9913667'
   constructor() { }
 
-  getToken(){
+  generateLink(clientId : string, redirectUri : string){
+    var scopes = 'user-read-private user-read-email user-library-read'
+    var link = 'https://accounts.spotify.com/authorize'+
+    '?response_type=code'+
+    '&client_id=' + clientId +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent(redirectUri) 
+
+    console.log(link);
+
   }
+
+  /*getToken(){
+
+  }*/
 }
