bd62821f4d1b5d881970d583a189dde24a093953
Petiazediter <petiazediter@gmail.com>
2021-03-20 18:39:13 +0100

    Add route to stats component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CallbackComponent } from './components/callback/callback.component';
 import { HomeComponent } from './components/home/home.component';
+import { StatsComponent } from './components/stats/stats.component';
 
 const routes: Routes = [
   {path : '', component : HomeComponent},
-  {path : 'callback', component : CallbackComponent} 
+  {path : 'callback', component : CallbackComponent},
+  {path : 'stats', component : StatsComponent}
 ];
 // path, component
 @NgModule({
