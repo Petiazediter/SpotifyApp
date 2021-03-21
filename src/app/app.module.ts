8682ce9103d0e5e69d50b42e142e1268af61825f
Petiazediter <petiazediter@gmail.com>
2021-03-21 12:51:46 +0100

    Add main text component for mobilty.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './components/home/home.component';
 import { IdComponent } from './components/static/id/id.component';
 import { CallbackComponent } from './components/callback/callback.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MainTextComponent } from './components/mini/main-text/main-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     IdComponent,
-    CallbackComponent
+    CallbackComponent,
+    MainTextComponent
   ],
   imports: [
     BrowserModule,
