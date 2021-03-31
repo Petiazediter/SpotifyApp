50f11dae0342c9796a77df23837ecec3de5e2a1c
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-31 14:17:50 +0200

    Create route for all stat component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllstatscomponentComponent } from './components/allstatscomponent/allstatscomponent.component';
 import { CallbackComponent } from './components/callback/callback.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path : '', component : HomeComponent},
-  {path : 'callback', component : CallbackComponent}
+  {path : 'callback', component : CallbackComponent},
+  {path : 'stats', component : AllstatscomponentComponent}
 ];
 // path, component
 @NgModule({
