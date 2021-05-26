7cf9819cb59b87587f2589a99cbcff3fd0f894ec
Peti <pverebes74@gmail.com>
2021-05-26 13:25:17 +0200

    Update Track.ts

--- src/app/models/Track.ts
+++ src/app/models/Track.ts
@@ -1,3 +1,14 @@
+import {AlbumArtist} from './AlbumArtist';
+import {Album} from './Album';
+
 export class Track{
-    
+  album: Album;
+  artists: AlbumArtist[];
+  explicit: boolean;
+  href: string;
+  id: string;
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
 }
