bf8ab07ca81b1cf92cc04b3da6234935189b2bf8
Peti <pverebes74@gmail.com>
2021-05-26 13:25:34 +0200

    Update Artist.ts

--- src/app/models/Artist.ts
+++ src/app/models/Artist.ts
@@ -1,3 +1,14 @@
-export class Artist{
-    
+import {Followers} from './Followers';
+import {Image} from './Image';
+
+export interface Artist {
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
 }
