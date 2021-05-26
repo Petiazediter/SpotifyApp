cfff3f88a10b0ddc6c766150231d4dbf97d52014
Peti <pverebes74@gmail.com>
2021-05-26 13:33:43 +0200

    Update getTrack and artist method in home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Track } from 'src/app/models/Track';
-import { TrackContainer } from 'src/app/models/TrackContainer';
-import { SpotifyService } from 'src/app/services/spotify.service';
-import { Terms } from 'src/app/services/terms';
+import {Component} from '@angular/core';
+import {SpotifyService} from 'src/app/services/spotify.service';
+import {Terms} from 'src/app/services/terms';
+import {Observable} from 'rxjs';
+import {ArtistWrapper} from '../../models/ArtistWrapper';
+import {TrackWrapper} from '../../models/TrackWrapper';
 
 @Component({
   selector: 'app-home',
@@ -11,74 +11,30 @@ import { Terms } from 'src/app/services/terms';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  loaded : number = 0;
-  private hadToLoad : number = 6;
-  isLoaded : boolean = false;
-  shortTermTopTracks!: any;
-  medTermTopTracks!: any;
-  longTermTopTracks!: any;
-  shortTermTopArtists!: any;
-  medTermTopArtists!:any;
-  longTermTopArtists!:any;  
+  terms = Terms;
 
-  constructor(public service : SpotifyService,private location : Location) { 
-    this.loaded = 0;
-    if ( SpotifyService.token != null){
-      this.processObserver(service.GetTopTrack(Terms.MED_TERM).toPromise(),this.SetMedTermTopTracks)
-      this.processObserver(service.GetTopTrack(Terms.LONG_TERM).toPromise(),this.SetLongTermTopTracks)
-      this.processObserver(service.GetTopTrack(Terms.SHORT_TERM).toPromise(),this.SetShortTermTopTracks)
-      this.processObserver(service.GetTopArtist(Terms.MED_TERM).toPromise(),this.SetMedTermTopArtists)
-      this.processObserver(service.GetTopArtist(Terms.LONG_TERM).toPromise(),this.SetLongTermTopArtists)
-      this.processObserver(service.GetTopArtist(Terms.SHORT_TERM).toPromise(),this.SetShortTermTopArtists)
-    }
+  constructor(public service: SpotifyService) {}
+
+  getTracksByTerm(term: Terms): Observable<TrackWrapper>{
+    return this.service.getTopTrack(term);
   }
 
-  SetMedTermTopTracks(y : HomeComponent,x : any){
-    y.medTermTopTracks = x;
+  getArtistByTerm(term: Terms): Observable<ArtistWrapper>{
+    return this.service.getTopArtist(term);
   }
 
-  SetMedTermTopArtists(y : HomeComponent, x : any){
-    y.medTermTopArtists = x;
-  }
-
-  SetLongTermTopTracks(y : HomeComponent,x : any){
-    y.longTermTopTracks = x;
-  }
-
-  SetLongTermTopArtists(y : HomeComponent, x : any){
-    y.longTermTopArtists = x;
-  }
-
-  SetShortTermTopArtists(y : HomeComponent, x : any){
-    y.shortTermTopArtists = x;
-  }
-
-  SetShortTermTopTracks(y : HomeComponent,x : any){
-    y.shortTermTopTracks = x;
-  }
-
-  processObserver(promise : Promise<any>, fn : (y:HomeComponent,x : any)=> void){
-    promise.then((value : any) => {
-      fn(this,value)
-    })
-    .finally(() =>{
-      this.loaded ++;
-      this.isLoaded = this.loaded == this.hadToLoad;
-    })
-  }
-
-  getToken() : string{
+  getToken(): string {
     return SpotifyService.token;
   }
 
-  onConnect(): void{
+  onConnect(): void {
     let href = window.location.href;
-    href = href + "callback/"
-    this.service.generateLink("aaeea3eaae8940c1a35e4645a2028096",href);
+    href = href + 'callback/';
+    this.service.generateLink('aaeea3eaae8940c1a35e4645a2028096', href);
   }
 
-  onSignOut() : void{
-    this.service.OnSignOut();
+  onSignOut(): void {
+    this.service.onSignOut();
   }
 
 }
